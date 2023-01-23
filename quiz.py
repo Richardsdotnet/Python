34a877fbb2e94ad18e9e74aa353893f4648524c6
Akaabiam Richards <112216210+Richardsdotnet@users.noreply.github.com>
2023-01-23 08:52:44 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,180 @@
+#write a quiz game
+#Ask a user to answer a few quiz questions
+#Display the question and the options to answer
+#Prompt the user to input the correct answer to the question
+#If the answer is correct,display correct, if the answer is wrong, display wrong
+
+user_selection = int(input("Enter 1 to start or 0 to exit"))
+
+while user_selection == 1:
+
+	print("Welcome to the quiz game")
+	print("Game on: Answer the questions below")
+	print("Q 1:What is the square root of 4?")
+	print("1. Ans: 4")	
+	print("2. Ans: 2")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+
+	print("Q 3:Do you have a PVC?")
+	print("1. Ans: Yes")
+	print("2. Ans: No")
+	answer_number_str = input("Type the answer")
+	if answer_number_str == "yes":
+		print("Make sure you vote")
+	elif answer_number_str == "Yes":
+		print("Make sure you vote")
+	elif answer_number_str == "YES":
+		print("Make sure you vote")
+	else:
+		print("Go get one")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+		print("Wrong")
+
+	print("Q 2:What is the sum of 2,4 and 6?")
+	print("1. Ans: 4")
+	print("2. Ans: 12")
+	print("3. Ans: 8")
+	answer_number_int = input("Enter number")
+	if answer_number_int == "2":
+		print("correct")
+	else:
+		print("Wrong")
+
+
+
+	user_selection = int(input("Do you wish to continue? yes, press 1 or 0 to exit"))
